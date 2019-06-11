8ee5071ee15375440f4b776a3dd0282aa075699d
Hodaka hashiba <b1015253@fun.ac.jp>
2019-06-11 15:59:00 +0900

    added rolling or not rolling button

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [state, dispatcher] = React.useState(0);
   return (
     <div className="App">
-      <div className="image-container">
+      <div className={`image-container ${state === 0 ? 'triangle' : (state === 1 ? '' : 'circle')}`} />
+      <div className="controller">
+        <button className="button" onClick={() => dispatcher(state + 1 <= 2 ? state + 1 : state)}>転がる</button>
+        <button className="button" onClick={() => dispatcher(state - 1 >= 0 ? state - 1 : state)}>転がらない</button>
       </div>
     </div>
   );
